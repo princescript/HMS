59fb90797d9b2996a4680253c048d31aaa17d8d7
Prince Singh <princesingh005958@gmail.com>
2026-02-16 13:12:51 +0530

    doctors

--- server/Controllers/DoctorController.cs
+++ server/Controllers/DoctorController.cs
@@ -2,6 +2,7 @@
 using server.DTOs;
 using server.DTOs.Doctor;
 using server.Models;
+using System.Linq.Expressions;
 
 namespace server.Controllers
 {
@@ -16,54 +17,78 @@ namespace server.Controllers
             _context = context;
         }
 
-        [HttpGet("GetAll",Name ="GetAllDoctors")]
-        public ActionResult<IEnumerable<DoctorDto>> GetAll()
+        [HttpGet("GetAll", Name = "GetAllDoctors")]
+        public ActionResult<Response<DoctorDto>> GetAll()
         {
-            var  doctors = _context.DbDoctor.Select(x =>
-            new DoctorDto {
+            var doctors = _context.DbDoctor.Select(x =>
+            new DoctorDto
+            {
                 DocId = x.DocId,
                 DocName = x.DocName,
                 DocPhone = x.DocPhone,
                 DocSpecialization = x.DocSpecialization,
             });
-            if (doctors == null||!doctors.Any())
+            if (doctors == null || !doctors.Any())
             {
-                return NoContent();
+                return Ok(new Response<object>
+                {
+                    Code = 200,
+                    Success = true,
+                    Message = "Doctors not  found.",
+                    Data = null,
+                    Pagination = null,
+
+                });
             }
             var res = new Response<object>
             {
                 Code = 200,
-                Sucess = true,
-                Message = "no msg",
-                Data = new {resultSet=doctors.ToList() },
+                Success = true,
+                Message = "All doctors data fetched successfully.",
+                Data = new { resultSet = doctors },
                 Pagination = null
             };
             return Ok(res);
         }
 
         [HttpGet("{id:int}", Name = "GetDoctorById")]
-        public ActionResult<DoctorDto> GetById(int id) 
+        public ActionResult<Response<DoctorDto>> GetById(int id)
         {
-            if(id <= 0)
+            if (id <= 0)
             {
                 return BadRequest();
             }
-            var foundDoctor = _context.DbDoctor.FirstOrDefault(x=>x.DocId == id);
+            var foundDoctor = _context.DbDoctor.FirstOrDefault(x => x.DocId == id);
 
-            if(foundDoctor == null)
+            if (foundDoctor == null)
             {
-                return NotFound("No doctor found.");
+                return NotFound(new Response<object>
+                {
+                    Code = 404,
+                    Success = false,
+                    Message = "Doctor not found",
+                    Data = null,
+                    Pagination = null
+                });
             }
-            return Ok(new DoctorDto
+            var doctorDto = new DoctorDto
             {
                 DocId = foundDoctor.DocId,
                 DocName = foundDoctor.DocName,
                 DocPhone = foundDoctor.DocPhone,
                 DocSpecialization = foundDoctor.DocSpecialization,
+            };
+            return Ok(new Response<object>
+            {
+                Code = 200,
+                Success = true,
+                Message = "Doctor Data fetched by id successfully.",
+                Data = new { resultSet = doctorDto },
+                Pagination = null
             });
         }
-        [HttpPost(Name ="CreateDoctor")]
-        public ActionResult<DoctorDto> Create([FromBody] DoctorDto entity)
+        [HttpPost(Name = "CreateDoctor")]
+        public ActionResult<Response<DoctorDto>> Create([FromBody] DoctorDto entity)
         {
             if (!ModelState.IsValid)
             {
@@ -77,44 +102,107 @@ namespace server.Controllers
             };
             _context.DbDoctor.Add(newDoctor);
             _context.SaveChanges();
-            entity.DocId = newDoctor.DocId;
-            return (entity);
+
+            var doctorDto = new DoctorDto
+            {
+                DocId = newDoctor.DocId,
+                DocName = newDoctor.DocName,
+                DocPhone = newDoctor.DocPhone,
+                DocSpecialization = newDoctor.DocSpecialization
+            };
+
+            return Ok(new Response<object>
+            {
+                Code = 201,
+                Success = true,
+                Message = "User created Sucessfully.",
+                Data = new { resultSet = doctorDto },
+                Pagination = null
+            });
         }
 
         [HttpPut("{id:int}", Name = "UpdateDoctorById")]
-        public ActionResult Update([FromBody] DoctorDto entity)
+        public ActionResult<Response<DoctorDto>> Update([FromBody] DoctorDto entity)
         {
-            if (!ModelState.IsValid) {
+            if (!ModelState.IsValid)
+            {
                 return BadRequest(ModelState);
             }
             var foundDoctor = _context.DbDoctor.FirstOrDefault(x => x.DocId == entity.DocId);
             if (foundDoctor == null)
             {
-                return NotFound("No doctor found.");
+                return NotFound(new Response<object>
+                {
+                    Code = 404,
+                    Success = true,
+                    Message = "Doctor not found.",
+                    Data = null,
+                    Pagination = null,
+
+                });
             }
             foundDoctor.DocName = entity.DocName;
             foundDoctor.DocPhone = entity.DocPhone;
             foundDoctor.DocSpecialization = entity.DocSpecialization;
             _context.SaveChanges();
-            return Ok("Doctor updated Successfully");
-       
+
+            var updatedDoctorDto = new DoctorDto
+            {
+                DocId = foundDoctor.DocId,
+                DocName = foundDoctor.DocName,
+                DocPhone = foundDoctor.DocPhone,
+                DocSpecialization = foundDoctor.DocSpecialization
+            };
+            return Ok(new Response<object>
+            {
+                Code = 200,
+                Success = true,
+                Message = "Doctor updated successfully.",
+                Data = new { resultSet = updatedDoctorDto },
+                Pagination = null,
+
+            });
+
         }
 
-        [HttpDelete("{id:int}",Name ="DeleteDoctorById")]
-        public ActionResult DeleteById(int id )
+        [HttpDelete("{id:int}", Name = "DeleteDoctorById")]
+        public ActionResult<Response<DoctorDto>> DeleteById(int id)
         {
-            if(id <= 0)
+            if (id <= 0)
             {
                 return BadRequest();
             }
-            var foundDoctor = _context.DbDoctor.FirstOrDefault(x=>x.DocId ==id);
-            if(foundDoctor == null)
+            var foundDoctor = _context.DbDoctor.FirstOrDefault(x => x.DocId == id);
+            if (foundDoctor == null)
             {
-                return NotFound("No doctor found");
+                return NotFound(new Response<object>
+                {
+                    Code = 404,
+                    Success = false,
+                    Message = "Doctor not found.",
+                    Data = null,
+                    Pagination = null
+
+                });
             }
             _context.DbDoctor.Remove(foundDoctor);
             _context.SaveChanges();
-            return Ok("Doctor deleted Sucessfully.");
+            var doctorDto = new DoctorDto
+            {
+                DocId = foundDoctor.DocId,
+                DocName = foundDoctor.DocName,
+                DocPhone = foundDoctor.DocPhone,
+                DocSpecialization = foundDoctor.DocSpecialization
+            };
+            return Ok(new Response<object>
+            {
+                Code = 200,
+                Success = true,
+                Message = "Doctor Deleted Successfully.",
+                Data = new { resultSet = doctorDto },
+                Pagination = null
+
+            });
         }
 
     }
--- server/DTOs/Response.cs
+++ server/DTOs/Response.cs
@@ -5,7 +5,7 @@ namespace server.DTOs
     public class Response<T>
     {
         public int? Code { get; set; } = null;
-        public bool Sucess { get; set; } = false;
+        public bool Success { get; set; } = false;
         public string Message { get; set; } = " ";
         public T? Data {  get; set; }
         public int? Pagination { get; set; } = null;
